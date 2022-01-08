686cca4a9dcd9e5d6f37953e3122af0142214143
Oğuzhan Kadaifçiler <oguzhan@oguzhankadaifciler.com>
2022-01-08 16:32:24 +0100

    Modify state variables and update ui

--- antDesignProSpaceX/src/components/SearchBox/SearchBox.tsx
+++ antDesignProSpaceX/src/components/SearchBox/SearchBox.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { PageContainer } from '@ant-design/pro-layout';
+import React, { useState, useEffect } from 'react';
 import { Typography, Row, Col, Input, InputNumber, Button } from 'antd';
 import { useIntl, FormattedMessage } from 'umi';
 import styles from './SearchBox.less';
@@ -12,25 +11,58 @@ const CodePreview: React.FC = ({ children }) => (
   </pre>
 );
 
-const SearchBox: React.FC = () => {
+
+const SearchBox: React.FC = ({ setFilterCount, setFilterText, setIsSubmitted }:
+  { setFilterCount: any, setFilterText: any, setIsSubmitted: any }) => {
   const intl = useIntl();
 
+  const [_filterCount, _setFilterCount] = useState(5);
+  const [_filterText, _setFilterText] = useState("");
+  const [_dynamicSearch, _setDynamicSearch] = useState(false);
+
+
+  const onSubmit = () => {
+    setFilterText(_filterText);
+    setFilterCount(_filterCount);
+  }
+
+  const toggleDynamicSearch = () => {
+    _setDynamicSearch(!_dynamicSearch);
+    onSubmit();
+  }
+
+  const filterTextChanged = (value: string) => {
+    _setFilterText(value);
+    if (_dynamicSearch) setFilterText(value);
+  }
+
+  const filterNumberChanged = (value: number) => {
+    _setFilterCount(value);
+    if (_dynamicSearch) setFilterCount(value);
+  }
 
   return (
-    <Row justify="end">
+    <Row justify="space-between">
 
       <Row className={styles.filterRowStyle}>
         <Col span={18}>
-          <Input placeholder="Search..." onChange={(event) => console.log(event.target.value)} />
+          <Input placeholder="Search..." value={_filterText} onChange={(event) => { filterTextChanged(event.target.value) }} />
 
         </Col>
         <Col span={6}>
-          <InputNumber min={1} max={10} defaultValue={5} onChange={(value) => console.log(value)} />
+          <InputNumber min={1} max={10} defaultValue={5} value={_filterCount} onChange={(value) => { filterNumberChanged(value) }} />
         </Col>
       </Row>
-      <Row className={styles.searchRowStyle}>
-        <Col >
-          <Button type="primary" onClick={() => this.props.clikedSubmitSearch}>Submit</Button>
+      <Row justify="space-between" className={styles.searchRowStyle}>
+        <Col>
+
+          <Button type="primary" onClick={() => toggleDynamicSearch()}>
+            {_dynamicSearch ? "Activate Static Search" : "Activate Dynamic Search"}
+          </Button>
+
+        </Col>
+        <Col>
+          {_dynamicSearch ? <></> : <Button type="primary" onClick={() => onSubmit()}>Submit</Button>}
         </Col>
       </Row>
     </Row>
