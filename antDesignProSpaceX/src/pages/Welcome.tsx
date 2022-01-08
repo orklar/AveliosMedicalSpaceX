1ab9db7bf4cbc54f85772a80fe0369bd23eced66
Oğuzhan Kadaifçiler <oguzhan@oguzhankadaifciler.com>
2022-01-08 11:06:00 +0100

    Update Welcome page

--- antDesignProSpaceX/src/pages/Welcome.tsx
+++ antDesignProSpaceX/src/pages/Welcome.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Card, Alert, Typography } from 'antd';
+import { Card, Row, Col, Typography } from 'antd';
 import { useIntl, FormattedMessage } from 'umi';
+import SearchBox from '../components/SearchBox/SearchBox';
 import styles from './Welcome.less';
+import InfoList from '../components/InfoList/InfoList';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
+
+
+const client = new ApolloClient({
+  uri: 'http://api.spacex.land/graphql/',
+  cache: new InMemoryCache(),
+});
 
 const CodePreview: React.FC = ({ children }) => (
   <pre className={styles.pre}>
@@ -12,12 +21,13 @@ const CodePreview: React.FC = ({ children }) => (
   </pre>
 );
 
-const Welcome: React.FC = () => {
-  const intl = useIntl();
-
-  return (
-    <PageContainer>
-      <Card>
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ *  <Card>
         <Alert
           message={intl.formatMessage({
             id: 'pages.welcome.alertMessage',
@@ -59,8 +69,33 @@ const Welcome: React.FC = () => {
         </Typography.Text>
         <CodePreview>yarn add @ant-design/pro-layout</CodePreview>
       </Card>
-    </PageContainer>
+
+      **/
+
+
+
+const Welcome: React.FC = () => {
+  const intl = useIntl();
+
+  return (
+    <ApolloProvider client={client}>
+      <PageContainer>
+        <Row>
+          <Col span={6}>
+            <Card className={styles.cardStyling} title="Search" extra={<a href="#">More</a>}>
+              <SearchBox></SearchBox>
+            </Card>
+          </Col>
+          <Col span={18}>
+            <Card className={styles.cardStyling} title="Results" extra={<a href="#">More</a>} >
+              <InfoList />
+            </Card>
+          </Col>
+        </Row>
+      </PageContainer>
+    </ApolloProvider>
   );
 };
 
 export default Welcome;
+
