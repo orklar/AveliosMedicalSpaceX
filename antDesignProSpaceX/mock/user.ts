6166989014f21fccb499805564835782ab2ac83b
Oğuzhan Kadaifçiler <oguzhan@oguzhankadaifciler.com>
2022-01-08 09:44:33 +0100

    Update user.ts
    Update user information and copyright text

--- antDesignProSpaceX/mock/user.ts
+++ antDesignProSpaceX/mock/user.ts
@@ -44,13 +44,13 @@ export default {
     res.send({
       success: true,
       data: {
-        name: 'Serati Ma',
+        name: 'Oğuzhan Kadaifçiler',
         avatar: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
         userid: '00000001',
-        email: 'antdesign@alipay.com',
-        signature: '海纳百川，有容乃大',
-        title: '交互专家',
-        group: '蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED',
+        email: 'legal@oguzhankadaifciler.com',
+        signature: 'ORKLAR',
+        title: 'Computer Engineer & Psychologist',
+        group: 'Space X Task for Avelios Medical',
         tags: [
           {
             key: '0',
